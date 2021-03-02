eb9270d0c7322071c1ce8767d0eef6fecc2907f8
David Ulloa <dulloa6310@gmail.com>
2021-03-02 18:16:54 -0500

    Created GameStateManager.java

---
+++ core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,25 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    protected void update(float dt) {
+
+    }
+
+    @Override
+    protected void render(SpriteBatch sb) {
+
+    }
+}
