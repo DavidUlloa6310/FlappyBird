b3fc15d1ac43f738b1bcd64fe2d247ddedd695d4
David Ulloa <41076449+DavidUlloa6310@users.noreply.github.com>
2021-03-05 13:36:46 -0500

    Finished collision and animations.

--- core/src/com/mygdx/game/sprites/Bird.java
+++ core/src/com/mygdx/game/sprites/Bird.java
@@ -1,6 +1,8 @@
 package com.mygdx.game.sprites;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class Bird {
@@ -9,6 +11,9 @@ public class Bird {
 
     private Vector3 position;
     private Vector3 velocity;
+    private Rectangle bounds;
+    private Animation birdAnimation;
+    private Texture texture;
 
     private Texture bird;
 
@@ -16,9 +21,14 @@ public class Bird {
         position = new Vector3(x, y, 0);
         velocity = new Vector3(0, 0, 0);
         bird = new Texture("bird.png");
+        texture = new Texture("birdanimation.png");
+        birdAnimation = new Animation(new TextureRegion(texture), 3, 0.5f);
+        bounds = new Rectangle(x, y, texture.getWidth() / 3, texture.getHeight());
     }
 
     public void update(float dt) {
+        birdAnimation.update(dt);
+
         if (position.y > 0) {
             velocity.add(0, GRAVITY, 0);
         }
@@ -32,17 +42,26 @@ public class Bird {
         }
 
         velocity.scl(1 / dt);
+        bounds.setPosition(position.x, position.y);
     }
 
     public Vector3 getPosition() {
         return position;
     }
 
-    public Texture getBird() {
-        return bird;
+    public TextureRegion getTexture() {
+        return birdAnimation.getFrame();
     }
 
     public void jump() {
-        velocity.y = 250;
+        velocity.y = 225;
+    }
+
+    public Rectangle getBounds() {
+        return bounds;
+    }
+
+    public void dispose() {
+        texture.dispose();
     }
 }
